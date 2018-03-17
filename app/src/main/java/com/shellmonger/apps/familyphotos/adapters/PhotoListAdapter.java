8ca1ee4ace6d9e736ee0add079718fdd9df9bdd8
Adrian Hall <photoadrian@outlook.com>
2018-03-17 14:48:03 -0700

    Issue 13 - added a CardView

--- app/src/main/java/com/shellmonger/apps/familyphotos/adapters/PhotoListAdapter.java
+++ app/src/main/java/com/shellmonger/apps/familyphotos/adapters/PhotoListAdapter.java
@@ -2,10 +2,12 @@ package com.shellmonger.apps.familyphotos.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shellmonger.apps.familyphotos.R;
@@ -15,6 +17,7 @@ import com.shellmonger.apps.familyphotos.repositories.RepositoryChange;
 import com.shellmonger.apps.familyphotos.repositories.RepositoryException;
 import com.shellmonger.apps.familyphotos.repositories.RepositoryFactory;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -99,17 +102,57 @@ public class PhotoListAdapter extends RecyclerView.Adapter<PhotoListAdapter.View
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public View mView;
 
-        private TextView mPhotoId;
+        private TextView mCaption, mAlbum, mLocation, mTags;
+        private ImageView mImage, mLocationIcon, mTagsIcon;
 
         public ViewHolder(View v) {
             super(v);
             mView = v;
 
-            mPhotoId = v.findViewById(R.id.listitem_photoid);
+            mCaption = v.findViewById(R.id.text_caption);
+            mAlbum = v.findViewById(R.id.text_album);
+            mLocation = v.findViewById(R.id.text_location);
+            mTags = v.findViewById(R.id.text_tags);
+
+            mImage = v.findViewById(R.id.image_picture);
+            mLocationIcon = v.findViewById(R.id.icon_location);
+            mTagsIcon = v.findViewById(R.id.icon_tags);
         }
 
         public void setModel(Photo model) {
-            mPhotoId.setText(model.getId());
+            // Set the caption (if available)
+            if (model.getCaption() != null) {
+                mCaption.setText(model.getCaption());
+                mCaption.setVisibility(View.VISIBLE);
+            } else {
+                mCaption.setVisibility(View.INVISIBLE);
+            }
+
+            // Set the image
+            mImage.setImageBitmap(model.getPicture());
+            mImage.setAdjustViewBounds(true);
+
+            // Set the album name
+            if (model.getAlbumId() != null) {
+                mAlbum.setText(model.getAlbumId());
+            } else {
+                mAlbum.setText(R.string.default_album_name);
+            }
+
+            // Set the Location - location is not available yet
+            mLocationIcon.setVisibility(View.INVISIBLE);
+            mLocation.setVisibility(View.INVISIBLE);
+
+            // Set the tags
+            List<String> tags = model.getTags();
+            if (tags == null || tags.size() == 0) {
+                mTagsIcon.setVisibility(View.INVISIBLE);
+                mTags.setVisibility(View.INVISIBLE);
+            } else {
+                mTagsIcon.setVisibility(View.VISIBLE);
+                mTags.setVisibility(View.VISIBLE);
+                mTags.setText(TextUtils.join(", ", tags));
+            }
         }
     }
 
