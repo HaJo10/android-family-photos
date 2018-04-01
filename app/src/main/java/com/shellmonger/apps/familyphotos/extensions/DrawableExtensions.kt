1d97bd624a386b54e5ebf60685d736c2d7f16a32
Adrian Hall <photoadrian@outlook.com>
2018-04-01 15:58:47 -0700

    Removed the app

--- app/src/main/java/com/shellmonger/apps/familyphotos/extensions/DrawableExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.shellmonger.apps.familyphotos.extensions
-
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
-
-fun Drawable.setIconColor(color: Int) {
-    mutate()
-    setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
-}
--- app/src/main/java/com/shellmonger/apps/familyphotos/lifecycle/RequestCodes.kt
+++
@@ -1,7 +0,0 @@
-package com.shellmonger.apps.familyphotos.lifecycle
-
-class RequestCodes {
-    companion object {
-        val TAKE_PICTURE = 1001
-    }
-}
