026b944e83f12268ba7da5bd418c65342d8daba3
Adrian Hall <photoadrian@outlook.com>
2018-04-01 18:00:15 -0700

    Dependencies updates.

---
+++ app/src/main/java/com/shellmonger/apps/familyphotos/ui/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.shellmonger.apps.familyphotos.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
