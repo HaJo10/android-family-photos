eb85f2cb9b2c3e38f3af4ef0c4ad085ba8764ab2
Adrian Hall <photoadrian@outlook.com>
2018-04-02 22:20:48 -0700

    Removed Dagger

---
+++ app/src/main/java/com/shellmonger/apps/familyphotos/lifecycle/ApplicationWrapper.kt
@@ -0,0 +1,9 @@
+package com.shellmonger.apps.familyphotos.lifecycle
+
+import android.app.Application
+
+class ApplicationWrapper : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/shellmonger/apps/familyphotos/ui/SplashActivity.kt
+++ app/src/main/java/com/shellmonger/apps/familyphotos/ui/SplashActivity.kt
@@ -2,10 +2,12 @@ package com.shellmonger.apps.familyphotos.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.shellmonger.apps.familyphotos.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
     }
 }
